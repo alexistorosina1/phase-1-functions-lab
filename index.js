b2a15bc101f62b612d79eb7ad4373825e501ad54
Alexis Torosina <alexistorosina1@gmail.com>
2022-09-22 14:59:15 -0400

    commit tree f1badff11186f794b57b501844b44d2fa7004d64

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    const headquarters = 42;
+    if (street > 42) {
+        return street-headquarters
+    }
+    else {
+        return headquarters-street
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    }
+    else {
+        return (destination - start) *264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) *.02
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2500) {
+        return 25
+    }
+    else {
+        return "cannot travel that far"
+    }
+}
